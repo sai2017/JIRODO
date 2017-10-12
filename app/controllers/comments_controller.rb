b16e0d6f2f2df35f25a64375afd9a17d64dc698d
佐々木皓晃 <ssk.hrak123@gmail.com>
2017-10-12 18:48:39 +0900

    “コメント削除編集機能実装”

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,36 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to review_path(@review), notice: 'コメントを投稿しました。' }
+        format.js { render :index }
       else
         format.html { render :new }
       end
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @review = @comment.review
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+     @comment.destroy
+     format.html { redirect_to review_path(@review) }
+      flash.now[:notice] = "コメントが削除されました!"
+    # JS形式でレスポンスを返します。
+     format.js { render :index }
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to review_path(@comment.review), notice: "コメントを更新しました"
+    else
+      render 'edit'
     end
   end
 
