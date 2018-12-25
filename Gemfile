9771e4e0c177819ed9ad02d29eecda718ff3b85b
佐々木皓晃 <ssk.hrak123@gmail.com>
2018-12-25 20:23:29 +0900

    migration_db_to_mysql

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem 'rails', '4.2.3'
-gem 'pg', '~> 0.20.0'
+gem 'mysql2', '~> 0.3.20'
+# gem 'pg', '~> 0.20.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -9,6 +10,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'rb-readline'
 
 group :development, :test do
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -13,89 +13,86 @@
 
 ActiveRecord::Schema.define(version: 20180215120310) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "review_id"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",    limit: 4
+    t.integer  "review_id",  limit: 4
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   add_index "comments", ["review_id"], name: "index_comments_on_review_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "contacts", force: :cascade do |t|
-    t.text     "name"
-    t.string   "email"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "name",       limit: 65535
+    t.string   "email",      limit: 255
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "jiro_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",    limit: 4, null: false
+    t.integer  "jiro_id",    limit: 4, null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   add_index "favorites", ["jiro_id"], name: "index_favorites_on_jiro_id", using: :btree
   add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "jiros", force: :cascade do |t|
-    t.string   "shop_name"
-    t.text     "content"
-    t.string   "shop_address"
-    t.string   "telephone_number"
-    t.text     "nearest_station"
-    t.text     "hours"
-    t.string   "holiday"
-    t.text     "menu"
-    t.text     "note"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "image"
-    t.integer  "favorites_count"
-    t.float    "latitude"
-    t.float    "longitude"
+    t.string   "shop_name",        limit: 255
+    t.text     "content",          limit: 65535
+    t.string   "shop_address",     limit: 255
+    t.string   "telephone_number", limit: 255
+    t.text     "nearest_station",  limit: 65535
+    t.text     "hours",            limit: 65535
+    t.string   "holiday",          limit: 255
+    t.text     "menu",             limit: 65535
+    t.text     "note",             limit: 65535
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "image",            limit: 255
+    t.integer  "favorites_count",  limit: 4
+    t.float    "latitude",         limit: 24
+    t.float    "longitude",        limit: 24
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "jiro_id"
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "image"
+    t.integer  "user_id",    limit: 4
+    t.integer  "jiro_id",    limit: 4
+    t.string   "title",      limit: 255
+    t.text     "content",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "image",      limit: 255
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.boolean  "admin",                  default: false
-    t.string   "uid",                    default: "",    null: false
-    t.string   "provider",               default: "",    null: false
-    t.string   "image_url"
-    t.string   "avatar"
-    t.string   "confirmation_token"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.boolean  "admin",                              default: false
+    t.string   "uid",                    limit: 255, default: "",    null: false
+    t.string   "provider",               limit: 255, default: "",    null: false
+    t.string   "image_url",              limit: 255
+    t.string   "avatar",                 limit: 255
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.string   "name"
+    t.string   "unconfirmed_email",      limit: 255
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
